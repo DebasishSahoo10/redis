d1a7149c2564ddfd4aa65b7df6ba4f51f72b03ea
DebasishSahoo10 <debasish@capitalcompute.com>
2026-06-07 21:39:30 +0530

    Added basic setup for redis and mongo endpoints in hono

---
+++ 02-Setup-Redis/src/index.ts
@@ -0,0 +1,33 @@
+import Redis from "ioredis";
+import mongoose from "mongoose";
+import {Hono} from "hono";
+
+const app = new Hono();
+
+const mongourl = "mongodb://localhost:27017/mongo_for_redis";
+
+
+app.get('/redis', async (c) => {
+    const reply = await redis.ping();
+    return c.json({
+        ok: true,
+        redis: reply
+    })
+})
+
+app.get('/mongo', async (c) => {
+
+    if (mongoose.connection.readyState === 0) {
+        await mongoose.connect(mongourl);
+    }
+
+    return c.json({
+        ok: true,
+        mongo: 'connected',
+        database: mongoose.connection.name
+    })
+})
+
+const redis = new Redis("redis://localhost:6379");
+
+export default app;
